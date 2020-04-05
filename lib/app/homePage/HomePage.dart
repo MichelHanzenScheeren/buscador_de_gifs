1d3cf867e417ded3d1fbbb85bf2dd18335d40d85
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2020-04-05 20:50:50 -0300

    Adição dos métodos de obtenção dos gifs da api.

--- lib/app/homePage/HomePage.dart
+++ lib/app/homePage/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -6,8 +8,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _search = "";
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+    if(_search.isEmpty) {
+      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=MHNHi4nQy2AEQfeW23BVysMSNRBzecCj&limit=20&rating=G");
+    } else {
+      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=MHNHi4nQy2AEQfeW23BVysMSNRBzecCj&q=$_search&limit=20&offset=$_offset&rating=G&lang=pt");
+    }
+    return json.decode(response.body);
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    _getGifs().then((data) {
+      print(data);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset("assets/images/appBar.gif"),
+        //title: Image.network("https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"), // (get by net)
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+    );
   }
 }
